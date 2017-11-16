8b4c954ccfe59fcef5b6774463009bb90c5fcb0a
Wren <Russell.Wren@us.sogeti.com>
2017-11-16 15:47:49 -0500

    URL Params Added to Deck Page

--- src/app/deck/deck.component.ts
+++ src/app/deck/deck.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+import 'rxjs/add/operator/map'
 
 @Component({
   selector: 'app-deck',
@@ -7,9 +10,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DeckComponent implements OnInit {
 
-  constructor() { }
+  public deckId;
+
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit() {
+    this.route.params
+      .map((params) => params['id'])
+      .subscribe((id) => {
+          this.deckId = id;
+      });
   }
 
 }
